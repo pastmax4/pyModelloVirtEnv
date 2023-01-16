9024b61ebf6de9120b06cf1261917f9392639d35
Massimo Pasteris <massimo.pasteris@unito.it>
2023-01-16 19:35:18 +0100

    ok

---
+++ .venv/mieLibrerie/provaLib.py
@@ -0,0 +1,4 @@
+class quadrato:
+    def __init__(self, lato):
+        self.perimetro = 4 * lato
+        self.area = lato * lato
---
+++ .venv/provaMain.py
@@ -0,0 +1,11 @@
+from mieLibrerie import provaLib 
+
+print("Ciao da Massimo")
+q1=provaLib.quadrato(7)
+print(q1.area)
+print("--- Fine ---")
+
+
+
+
+
